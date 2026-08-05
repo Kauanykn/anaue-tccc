64065659acf4948fa6701b99a218324e91665a9f
kauanykn <kauanyneves839@gmail.com>
2026-08-05 18:20:30 -0300

    05/08

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,87 @@
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+
+<footer class="footer">
+
+    <div class="footer__container">
+
+        <div class="footer__sobre">
+
+            <h3>Anauê</h3>
+
+            <p>
+                O buffet infantil mais divertido de Tatuí!
+            </p>
+
+            <p>
+                Realizamos sonhos e criamos
+                memórias inesquecíveis.
+            </p>
+
+        </div>
+
+        <div class="footer__contato">
+
+            <div class="footer__item">
+                <i class="fa-solid fa-phone"></i>
+
+                <span>(15) 3259-7977 (WhatsApp e Fixo)</span>
+            </div>
+
+            <div class="footer__item">
+                <i class="fa-regular fa-envelope"></i>
+
+                <span>anaueespaco@gmail.com</span>
+            </div>
+
+            <div class="footer__item">
+                <i class="fa-solid fa-location-dot"></i>
+
+                <span>Av. Dr. Sales Gomes, 587 - Centro</span>
+            </div>
+
+            <div class="footer__item">
+                <i class="fa-solid fa-city"></i>
+
+                <span>Tatuí - SP</span>
+            </div>
+
+        </div>
+
+        <div class="footer__social">
+
+            <h3>Siga-nos</h3>
+
+            <p>
+                @anaue.espaco
+            </p>
+
+            <span>
+                Acompanhe nossas festas e novidades!
+            </span>
+
+            <div class="footer__redes">
+
+                <a href="#">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="footer__linha"></div>
+
+    <div class="footer__copy">
+
+        <p>© 2026 Buffet Infantil Anauê</p>
+
+        <span>
+            Feito com amor para festas inesquecíveis
+        </span>
+
+    </div>
+
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anauê Espaço Infantil</title>
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    @stack('styles')
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/home.css') }}" rel="stylesheet">
 </head>
@@ -13,5 +17,9 @@
     <main>
         @yield('content')
     </main>
+    <footer>
+         @include('components.footer')
+    </footer>
+
 </body>
 </html>
